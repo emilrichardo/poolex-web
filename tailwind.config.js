35d4417ebe2c711455273c9ee720da5e2084b701
emilrichardo <emilrichardo@gmail.com>
2023-03-08 15:18:32 -0300

    tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,84 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`
+    }
+    return `rgb(var(${variableName}))`
+  }
 }
+
+module.exports = {
+  darkMode: 'class',
+    content: [
+      "./components/**/*.{js,vue,ts}",
+      "./layouts/**/*.vue",
+      "./pages/**/*.vue",
+      "./plugins/**/*.{js,ts}",
+      "./nuxt.config.{js,ts}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          'body': "var(--font-base)",
+          'heading': "var(--font-heading)",
+          'fantasy':"var(--font-fantasy)"
+        },
+        colors:{
+          accesible:{
+            primary : withOpacity("--accesible-primary"),
+            secondary : withOpacity("--accesible-secondary"),
+          },
+
+
+          primary:{
+            DEFAULT: withOpacity("#9a00ff"),
+            100:  withOpacity("--primary-lighten-300"),
+            200:  withOpacity("--primary-lighten-200"),
+            300:  withOpacity("--primary-lighten-100"),
+            400:  withOpacity("--primary"),
+            500:  withOpacity("--primary-darken-100"),
+            600:  withOpacity("--primary-darken-200"),
+            700:  withOpacity("--primary-darken-300"),
+          },
+          secondary:{
+            DEFAULT: withOpacity("--secondary"),
+            100:  withOpacity("--secondary-lighten-300"),
+            200:  withOpacity("--secondary-lighten-200"),
+            300:  withOpacity("--secondary-lighten-100"),
+            400:  withOpacity("--secondary"),
+            500:  withOpacity("--secondary-darken-100"),
+            600:  withOpacity("--secondary-darken-200"),
+            700:  withOpacity("--secondary-darken-300"),
+          },
+          dark:{
+            DEFAULT: withOpacity("--dark"),
+            100:  withOpacity("--dark-lighten-300"),
+            200:  withOpacity("--dark-lighten-200"),
+            300:  withOpacity("--dark-lighten-100"),
+            400:  withOpacity("--dark"),
+            500:  withOpacity("--dark-darken-100"),
+            600:  withOpacity("--dark-darken-200"),
+            700:  withOpacity("--dark-darken-300"),
+          },
+          light:{
+            DEFAULT: withOpacity("--light"),
+            100:  withOpacity("--light-lighten-300"),
+            200:  withOpacity("--light-lighten-200"),
+            300:  withOpacity("--light-lighten-100"),
+            400:  withOpacity("--light"),
+            500:  withOpacity("--light-darken-100"),
+            600:  withOpacity("--light-darken-200"),
+            700:  withOpacity("--light-darken-300"),
+          },
+        },
+        borderRadius: {
+          'none': 'var(--rounded-none)',
+          'sm': 'var(--rounded-sm)',
+          DEFAULT: 'var(--rounded)',
+          'md': 'var(--rounded-md)',
+          'lg': 'var(--rounded-lg)',
+        },
+      },
+    },
+    plugins: [],
+  }
