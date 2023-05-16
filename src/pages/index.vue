aff159a4df01bf98141ca250ca09ab21fb64212c
emilrichardo <emilrichardo@gmail.com>
2023-05-16 17:52:39 -0300

    product is active

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,29 +1,31 @@
 <template>
   <Carousel :wrap-around="true" :autoplay="4000">
-    <Slide v-for="product in globalData.products" :key="product">
-      <Hero
-        :title="useGetLocale(locale, product).name"
-        :caption="useGetLocale(locale, product).description"
-        :background="product.attributes.Featured_image.data?.attributes.url"
-      >
-        <template #cta>
-          <div class="flex gap-4">
-            <NuxtLink
-              v-if="product.attributes.register_link"
-              :to="product.attributes.register_link"
-              ><Button type="button" variant="primary_shadow">{{
-                locale === "es" ? "Registrate" : "Register"
-              }}</Button></NuxtLink
-            >
-            <nuxt-link :to="localePath('/' + product.attributes.slug)">
-              <Button type="button" variant="default">{{
-                locale === "es" ? "Ver más" : "View more"
-              }}</Button>
-            </nuxt-link>
-          </div>
-        </template>
-      </Hero>
-    </Slide>
+    <template v-for="product in globalData.products" :key="product">
+      <Slide v-if="product.attributes.active">
+        <Hero
+          :title="useGetLocale(locale, product).name"
+          :caption="useGetLocale(locale, product).description"
+          :background="product.attributes.Featured_image.data?.attributes.url"
+        >
+          <template #cta>
+            <div class="flex gap-4">
+              <NuxtLink
+                v-if="product.attributes.register_link"
+                :to="product.attributes.register_link"
+                ><Button type="button" variant="primary_shadow">{{
+                  locale === "es" ? "Registrate" : "Register"
+                }}</Button></NuxtLink
+              >
+              <nuxt-link :to="localePath('/' + product.attributes.slug)">
+                <Button type="button" variant="default">{{
+                  locale === "es" ? "Ver más" : "View more"
+                }}</Button>
+              </nuxt-link>
+            </div>
+          </template>
+        </Hero>
+      </Slide>
+    </template>
 
     <template #addons>
       <div class="absolute bottom-20 w-full">
