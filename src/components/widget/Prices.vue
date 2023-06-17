dc9620c370a941c6c5b8b8f65a9eb810b5bf6885
Sebastián <37385046+Shini-D@users.noreply.github.com>
2023-06-16 19:18:27 -0500

    Stocks

--- src/components/widget/Prices.vue
+++ src/components/widget/Prices.vue
@@ -33,60 +33,12 @@
 </template>
 <script setup>
 const { type } = defineProps(["type"]);
-const dataStock = [
-  {
-    icon: "mdi:apple",
-    name: "Apple",
-    price: 1234,
-    dif: -0.12,
-  },
-  {
-    icon: "mdi:netflix",
-    name: "Netflix",
-    symbol: "eth",
-    price: 7835,
-    dif: 3.2,
-  },
-  {
-    icon: "mdi:uber",
-    name: "Uber",
-    price: 0.992,
-    dif: -0.02,
-  },
-  {
-    icon: "mdi:apple",
-    name: "Apple",
-    price: 1234,
-    dif: -0.12,
-  },
-  {
-    icon: "mdi:netflix",
-    name: "Netflix",
-    symbol: "eth",
-    price: 7835,
-    dif: 3.2,
-  },
-  {
-    icon: "mdi:uber",
-    name: "Uber",
-    price: 0.992,
-    dif: -0.02,
-  },
-];
 
 const priceData = computed(() => {
   if (type === "crypto") {
     return;
   }
 });
-
-const iconApple = `<svg fill="#000000" width="800px" height="800px" viewBox="-52.01 0 560.035 560.035" xmlns="http://www.w3.org/2000/svg">
-<g id="SVGRepo_bgCarrier" stroke-width="0"/>
-<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
-<g id="SVGRepo_iconCarrier">
-<path d="M380.844 297.529c.787 84.752 74.349 112.955 75.164 113.314-.622 1.988-11.754 40.191-38.756 79.652-23.343 34.117-47.568 68.107-85.731 68.811-37.499.691-49.557-22.236-92.429-22.236-42.859 0-56.256 21.533-91.753 22.928-36.837 1.395-64.889-36.891-88.424-70.883-48.093-69.53-84.846-196.475-35.496-282.165 24.516-42.554 68.328-69.501 115.882-70.192 36.173-.69 70.315 24.336 92.429 24.336 22.1 0 63.59-30.096 107.208-25.676 18.26.76 69.517 7.376 102.429 55.552-2.652 1.644-61.159 35.704-60.523 106.559M310.369 89.418C329.926 65.745 343.089 32.79 339.498 0 311.308 1.133 277.22 18.785 257 42.445c-18.121 20.952-33.991 54.487-29.709 86.628 31.421 2.431 63.52-15.967 83.078-39.655"/>
-</g>
-</svg>`;
 
 const stockData1 = ref(null)
 const stockData2 = ref(null)
@@ -142,7 +94,6 @@ async function getStockData() {
         dif: stockData4.value?.data["Global Quote"]["09. change"]
       }
     ]
-    console.log(stocksAlpha)
 
   } catch (error) {
     console.log("Error al obtener los datos de stock:", error.message);
