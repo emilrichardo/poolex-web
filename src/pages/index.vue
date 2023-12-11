aa823694ec8b3ce5ee3db28b240d7d4ae96dfc01
Criss_Dev <cristian@xiclica.com>
2023-12-11 18:03:14 -0500

    change text title

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -44,8 +44,7 @@
       </NuxtLink>
     </div>
 
-    <Modal :id="`modal-id`" v-if="showModal && knowUsLink"
-      :title="locale === 'es' ? 'Conoce más de nosotros' : 'Learn more about us'" @closeModal="closeModal"
+    <Modal :id="`modal-id`" v-if="showModal && knowUsLink" :title="dataModal.title" @closeModal="closeModal"
       :color="dataModal.color">
       <div class="max-w-[94vw]">
         <iframe :src="getFullYoutubeLink(knowUsLink)" title=" YouTube video player" frameborder="0"
