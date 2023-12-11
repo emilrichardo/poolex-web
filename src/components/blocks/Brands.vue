50d1021407fbc5d8c2d18bf97d859df4d9baffbc
Criss_Dev <cristian@xiclica.com>
2023-12-11 11:27:16 -0500

    changes styles card

--- src/components/blocks/Brands.vue
+++ src/components/blocks/Brands.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="carousel-brands py-12">
-    <Carousel v-if="content.carousel_active" :breakpoints="breakpoints" :autoplay="2000" :wrap-around="true">
+    <Carousel v-if="content.carousel_active" :breakpoints="breakpoints" :autoplay="false" :wrap-around="true">
       <Slide v-for="logo in content.logo" :key="logo.id">
         <div class="carousel__item">
           <a :href="`${logo?.caption}`" target="_blank" rel="noopener noreferrer">
-            <div class="bg-light rounded-lg px-8 mx-4 py-8">
+            <div class="bg-light rounded-lg mx-4 p-6 flex justify-center items-center sm:min-h-[120px]">
               <NuxtImg v-if="logo.icon?.data?.attributes?.url" :src="logo.icon?.data?.attributes?.url" :alt="logo.title"
-                :class="`${imageSize[content.size]} `" />
+                class="h-full max-h-[100px] min-h-[90px] min-w-[90px] sm:min-h-0 " />
             </div>
           </a>
         </div>
